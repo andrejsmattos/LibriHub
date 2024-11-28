86609b306ef8f9fbee1bab29b913ba57951272c7
André <andrejusmattos@gmail.com>
2024-11-28 10:21:26 -0300

    Primeiro commit

---
+++ src/main/java/br/com/andrejsmattos/librihub/LibrihubApplication.java
@@ -0,0 +1,13 @@
+package br.com.andrejsmattos.librihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrihubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrihubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andrejsmattos/librihub/LibrihubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrejsmattos.librihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrihubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
